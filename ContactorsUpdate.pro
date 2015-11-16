5a203ae3afe1949896795401276df055d0e5ab43
yt2014 <taoy_2007@126.com>
2015-11-16 17:24:23 +0800

    init file

---
+++ ContactorsUpdate.pro
@@ -0,0 +1,18 @@
+#-------------------------------------------------
+#
+# Project created by QtCreator 2015-11-16T15:03:41
+#
+#-------------------------------------------------
+
+QT       += core
+
+QT       -= gui
+
+TARGET = ContactorsUpdate
+CONFIG   += console
+CONFIG   -= app_bundle
+
+TEMPLATE = app
+
+
+SOURCES += main.cpp
---
+++ main.cpp
@@ -0,0 +1,52 @@
+#include <QCoreApplication>
+#include <QDebug>
+
+int main(int argc, char *argv[])
+{
+    QCoreApplication a(argc, argv);
+
+    qDebug()<<"aaaa test";
+
+    QSqlDatabase db1 = QSqlDatabase::database("Dail",true);
+
+     if(db1.isValid()&&db1.isOpen())
+     {
+         QString strQuery = "select * from verification where hdnumber like \'%" + strDiskInfo.trimmed() +"%\'";
+
+         QSqlQuery query(db1);
+
+         query.exec(strQuery);
+
+
+
+         int countOfRecord = 0;
+         while(query.next())
+         {
+             countOfRecord = countOfRecord+1;
+         }
+         if(countOfRecord>0)
+         {
+            MainWindow w;
+            w.show();
+            file.close();
+            return a.exec();
+         }
+         else
+         {
+             QMessageBox::information(NULL,"NO","认证不通过");
+         }
+
+         db1.close();
+         file.close();
+         return 0;
+     }
+     else
+     {
+         //QMessageBox::information(NULL,"NO","数据库文件丢失");
+         //file.close();
+         return 0;
+     }
+
+
+    return a.exec();
+}
